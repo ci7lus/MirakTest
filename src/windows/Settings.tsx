edd3b3082f156680e61e015568c20490da6460b3
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-03-25 16:32:25 +0900

    :wrench: 設定ファイルを開けるように

--- src/windows/Settings.tsx
+++ src/windows/Settings.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { MirakurunSettingForm } from "../components/settings/Mirakurun"
 import { SayaSettingForm } from "../components/settings/Saya"
+import { store } from "../utils/store"
 
 type Routes = "Mirakurun" | "Saya"
 
@@ -24,7 +25,7 @@ export const Settings: React.VFC<{}> = () => {
       <div className="w-1/3 border-r border-gray-600 py-4 overflow-auto flex flex-col">
         <button
           type="button"
-          className={`p-4 border-b border-gray-400 text-left ${
+          className={`focus:outline-none p-4 border-b border-gray-400 text-left ${
             route === "Mirakurun" && "bg-blue-500"
           }`}
           onClick={() => setRoute("Mirakurun")}
@@ -33,13 +34,21 @@ export const Settings: React.VFC<{}> = () => {
         </button>
         <button
           type="button"
-          className={`p-4 border-b border-gray-400 text-left ${
+          className={`focus:outline-none p-4 border-b border-gray-400 text-left ${
             route === "Saya" && "bg-blue-500"
           }`}
           onClick={() => setRoute("Saya")}
         >
           Saya
         </button>
+        <div className="flex items-center justify-center">
+          <a
+            className="text-blue-400 hover:underline text-sm py-4"
+            onClick={() => store.openInEditor()}
+          >
+            設定ファイルを開く
+          </a>
+        </div>
       </div>
       <div className="h-full w-2/3 my-4p-4 text-gray-100 overflow-auto">
         <Router route={route} />
