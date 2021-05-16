8fa967f09b1e2bd7d9ba4575f26c26fc79d6084d
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-05-16 18:12:55 +0900

    :green_heart: Win向けビルドのelectron固定を解除

--- setup_wcjs.ps1
+++ setup_wcjs.ps1
@@ -1,9 +1,9 @@
 $ARCH = "x64"
 $OS_NAME = "win"
 $RUNTIME = "electron"
-$RUNTIME_VER = "11.1.0"
+$RUNTIME_VER = "12.0.7"
 $VLC_VER = "3.0.11"
-$WCJS_VER = "0.3.1"
+$WCJS_VER = "0.5.1"
 
 function InsertAfter ([string]$file, [string]$needle, [string]$text, [string]$encoding = "utf8NoBOM") {
     $data = Get-Content "$file"
@@ -18,12 +18,6 @@ function ReplaceOne ([string]$file, [string]$needle, [string]$text, [string]$enc
     $data = Get-Content "$file" | ForEach-Object { $_ -replace "$needle", "$text" }
     $data -Join "`n" | Out-File "$file" -Encoding "$encoding" -NoNewline
 }
-
-# Setup Electron 11
-yarn add -D --ignore-scripts electron@^11
-InsertAfter ".\electron-builder.yml" "afterPack:" "electronVersion: `"11.1.0`""
-ReplaceOne ".\package.json" "`"browserslist`": .+" "`"browserslist`": `"electron 11.1.0`""
-ReplaceOne ".\package.json" "`"runtimeVersion`": .+" "`"runtimeVersion`": `"11.1.0`""
 
 # Setup WebChimera.js
 Set-Location "node_modules"
