80bb7d57b9836caec005ed633419e2ef8fcb3b92
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-05-20 22:42:34 +0900

    :bento: Presence用局ロゴを追加

--- src/utils/presence.ts
+++ src/utils/presence.ts
@@ -1,10 +1,17 @@
 import type { Service } from "../infra/mirakurun/api"
 
+export const PresenceRegisteredGRLogos = [
+  1040, 1048, 1056, 1064, 1072, 17424, 17432, 17440, 17448, 23608,
+]
+
 export const PresenceRegisteredBSLogos = [
-  101, 103, 141, 151, 161, 171, 211, 222,
+  101, 103, 141, 151, 161, 171, 181, 191, 200, 211, 222,
 ]
 
 export const getServiceLogoForPresence = (service: Service) => {
+  if (PresenceRegisteredGRLogos.includes(service.serviceId)) {
+    return `gr_${service.serviceId}`
+  }
   if (PresenceRegisteredBSLogos.includes(service.serviceId)) {
     return `bs_${service.serviceId}`
   }
