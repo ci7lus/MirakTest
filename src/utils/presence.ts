64b2730799fb7ed2bf8a430a9fe9679d377e1f16
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-05-21 02:26:04 +0900

    :bento: 神奈川とBSの不足局ロゴを追加

--- src/utils/presence.ts
+++ src/utils/presence.ts
@@ -1,12 +1,13 @@
 import type { Service } from "../infra/mirakurun/api"
 
 export const PresenceRegisteredGRLogos = [
-  1040, 1048, 1056, 1064, 1072, 17424, 17432, 17440, 17448, 23608, 34840, 37904,
-  37912, 37920,
+  1040, 1048, 1056, 1064, 1072, 17424, 17432, 17440, 17448, 23608, 24632, 34840,
+  37904, 37912, 37920,
 ]
 
 export const PresenceRegisteredBSLogos = [
-  101, 103, 141, 151, 161, 171, 181, 191, 200, 211, 222,
+  101, 103, 141, 151, 161, 171, 181, 191, 200, 211, 222, 231, 234, 236, 241,
+  242, 251, 252, 255, 256,
 ]
 
 export const getServiceLogoForPresence = (service: Service) => {
