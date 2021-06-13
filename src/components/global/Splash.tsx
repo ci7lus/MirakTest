c52d7ab7198a918c37f4c245425deeb0b286be5f
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-06-13 21:18:16 +0900

    :lipstick: スプラッシュ画面のロゴ配置を修正

--- src/components/global/Splash.tsx
+++ src/components/global/Splash.tsx
@@ -9,9 +9,15 @@ export const Splash: React.FC<{}> = ({ children }) => {
     <div className="overflow-hidden select-none">
       <div className="w-full h-screen flex items-center justify-center">
         <div className="flex flex-col items-center justify-center space-y-4">
-          <div className="flex items-center justify-center space-x-8 mr-4">
-            <img width="100px" src="./assets/miraktest_logo.png" />
-            <img width="200px" src="./assets/miraktest_text.svg" />
+          <div className="flex items-center justify-center space-x-6 mr-4">
+            <img
+              style={{ width: "8vw", maxWidth: "100px", minWidth: "40px" }}
+              src="./assets/miraktest_logo.png"
+            />
+            <img
+              style={{ width: "16vw", maxWidth: "200px", minWidth: "80px" }}
+              src="./assets/miraktest_text.svg"
+            />
           </div>
           {children && (
             <div
