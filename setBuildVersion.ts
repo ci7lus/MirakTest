541f382a4cfa85af8f3c7e4cb8dc875471276d54
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-07-17 15:02:58 +0900

    :green_heart: ランダムでWindowsビルドが落ちるのを解消
    コミットハッシュが16進解釈されてしまう場合がある?

--- setBuildVersion.ts
+++ setBuildVersion.ts
@@ -1,4 +1,5 @@
 import fs from "fs"
+import pkg from "./package.json"
 
 const main = async () => {
   const sha1 = process.env.SHA1
@@ -6,9 +7,16 @@ const main = async () => {
   const yml = await fs.promises.readFile("./electron-builder.yml", "utf8")
   if (yml.includes("buildVersion"))
     throw new Error("already buildVersion included")
+  let buildVersion: string
+  if (process.env.IS_WINDOWS) {
+    const [version] = pkg.version.split("-")
+    buildVersion = `${version}.${sha1.slice(0, 7)}`
+  } else {
+    buildVersion = sha1
+  }
   const rewrited = yml.replace(
     "productName: MirakTest",
-    `productName: MirakTest\nbuildVersion: ${sha1}`
+    `productName: MirakTest\nbuildVersion: "${buildVersion}"`
   )
   await fs.promises.writeFile("./electron-builder.yml", rewrited)
 }
