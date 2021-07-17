449e4a48eb785fe7dfd1a0f0a15818bcad0536c7
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-07-17 20:26:47 +0900

    :lipstick: ロゴ濃度を控えめに

--- src/components/global/Splash.tsx
+++ src/components/global/Splash.tsx
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from "react"
 export const Splash: React.FC<{}> = ({ children }) => {
   const [opacity, setOpacity] = useState(0)
   useEffect(() => {
-    setOpacity(100)
+    setOpacity(1)
   }, [])
   return (
     <div className="overflow-hidden select-none">
       <div className="w-full h-screen flex items-center justify-center">
         <div className="flex flex-col items-center justify-center space-y-4">
-          <div className="flex items-center justify-center space-x-6 mr-4">
+          <div
+            className="flex items-center justify-center space-x-6 mr-4 transition-opacity duration-1000 ease-out"
+            style={{ opacity: 1.7 - opacity }}
+          >
             <img
               style={{ width: "8vw", maxWidth: "100px", minWidth: "40px" }}
               src="./assets/miraktest_logo.png"
