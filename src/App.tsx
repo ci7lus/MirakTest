4ed0726225803c229bd6a84e4729f986e1033aeb
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-07-18 21:18:33 +0900

    :art: 画面を離す際にアンマウント (#7)
    beforeunloadに引っ掛けてコンポーネントのアンロード処理を走らせる

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import ReactDOM from "react-dom"
 import { RecoilRoot } from "recoil"
 import { RecoilObserver } from "./components/global/RecoilObserver"
 import { Splash } from "./components/global/Splash"
@@ -8,7 +9,22 @@ import { Settings } from "./windows/Settings"
 
 const Router: React.VFC<{}> = () => {
   const hash = window.location.hash as "#MainPlayer" | "#Settings" | "#EPG"
-  if (hash === "#MainPlayer") {
+  const [unmounted, setUnmounted] = useState(false)
+  useEffect(() => {
+    const app = document.getElementById("app")
+    if (!app) return
+    const beforeUnload = () => {
+      setUnmounted(true)
+      ReactDOM.unmountComponentAtNode(app)
+    }
+    window.addEventListener("beforeunload", beforeUnload)
+    return () => {
+      window.removeEventListener("beforeunload", beforeUnload)
+    }
+  }, [])
+  if (unmounted) {
+    return <Splash />
+  } else if (hash === "#MainPlayer") {
     return <CoiledMainPlayer />
   } else if (hash === "#Settings") {
     return <Settings />
