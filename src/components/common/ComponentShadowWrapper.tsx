e5b99b6bff75f255f70f3dff169c2923d18ff059
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-09-29 19:48:03 +0900

    :art: リンクをブラウザで開くように

--- src/components/common/ComponentShadowWrapper.tsx
+++ src/components/common/ComponentShadowWrapper.tsx
@@ -1,3 +1,4 @@
+import { remote } from "electron"
 import React, { useEffect, useRef } from "react"
 import ReactDOM from "react-dom"
 
@@ -8,15 +9,27 @@ export const ComponentShadowWrapper: React.VFC<{
 }> = ({ Component, _id, className }) => {
   const ref = useRef<HTMLDivElement>(null)
   useEffect(() => {
+    const remoteWindow = remote.getCurrentWindow()
+    remoteWindow.webContents.setWindowOpenHandler(({ url }) => {
+      if (url === "about:blank") {
+        return { action: "allow" }
+      } else if (url.startsWith("http")) {
+        remote.shell.openExternal(url)
+        return { action: "deny" }
+      } else {
+        return { action: "deny" }
+      }
+    })
     const div = ref.current
-    if (!div) return
-    let root: ShadowRoot
-    if (div.shadowRoot) {
-      root = div.shadowRoot
-    } else {
-      root = div.attachShadow({ mode: "open" })
+    if (div) {
+      let root: ShadowRoot
+      if (div.shadowRoot) {
+        root = div.shadowRoot
+      } else {
+        root = div.attachShadow({ mode: "open" })
+      }
+      ReactDOM.render(<Component />, root)
     }
-    ReactDOM.render(<Component />, root)
   }, [ref, _id])
   return <div ref={ref} id={_id} className={className} />
 }
