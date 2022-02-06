a841d1c48d01207390146bf38e0c2942d0470f51
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-02-06 20:02:32 +0900

    🐛 長時間動かすとタイマーがずれるのを修正

--- src/hooks/date.ts
+++ src/hooks/date.ts
@@ -7,16 +7,15 @@ export const useNow = () => {
   const [now, setNow] = useState(dayjs())
 
   useEffect(() => {
-    let interval: null | NodeJS.Timeout = null
-    const update = () => setNow(dayjs())
-    let timeout: null | NodeJS.Timeout = setTimeout(() => {
+    const update = () => {
+      setNow(dayjs())
+      timeout = setTimeout(update, (60 - new Date().getSeconds()) * 1000)
+    }
+    let timeout = setTimeout(() => {
       update()
-      interval = setInterval(update, 60 * 1000)
-      timeout = null
     }, (60 - new Date().getSeconds()) * 1000)
     return () => {
       timeout && clearTimeout(timeout)
-      interval && clearInterval(interval)
     }
   }, [])
 
