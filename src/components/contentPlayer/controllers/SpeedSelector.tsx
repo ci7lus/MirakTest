827ac9a510195ec6771ca16f694d91ddbaf59b8e
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-02-09 16:57:15 +0900

    🚸 速度指定の受け入れにワンクッション入れて操作感を改善 (#26)

--- src/components/contentPlayer/controllers/SpeedSelector.tsx
+++ src/components/contentPlayer/controllers/SpeedSelector.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export const SpeedSelector = ({
   isSeekable,
@@ -10,23 +10,33 @@ export const SpeedSelector = ({
   speed: number
   setSpeed: React.Dispatch<React.SetStateAction<number>>
 }) => {
+  const [local, setLocal] = useState(speed)
+  const [isInvalid, setIsInvalid] = useState(false)
+  useEffect(() => {
+    if (local < 0.1 || local > 5) {
+      setIsInvalid(true)
+      return
+    }
+    setIsInvalid(false)
+    setSpeed(local)
+  }, [local])
   return (
     <input
       type="number"
       className={clsx(
         "block form-input rounded-md text-gray-200",
-        isSeekable ? "bg-gray-800" : "bg-gray-600"
+        isSeekable && !isInvalid ? "bg-gray-800" : "bg-gray-500"
       )}
-      value={speed}
+      value={local}
       step={0.1}
-      max={3}
-      min={1}
+      max={5}
+      min={0.1}
       onChange={(e) => {
         const value = parseFloat(e.target.value)
         if (Number.isNaN(value)) {
           return
         }
-        setSpeed(value)
+        setLocal(value)
       }}
       disabled={!isSeekable}
       onKeyDown={(e) => e.stopPropagation()}
