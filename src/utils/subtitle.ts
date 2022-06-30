32eb36974a44ff8dc71ee4db73fb7e879ebeb007
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-06-30 16:04:29 +0900

    :label: 型を整理

--- src/utils/subtitle.ts
+++ src/utils/subtitle.ts
@@ -1,13 +1,8 @@
-import { CanvasProvider } from "aribb24.js"
-
-type ProviderOption = Exclude<
-  Parameters<CanvasProvider["render"]>[0],
-  undefined
->
+import { CanvasProviderOption } from "aribb24.js"
 
 export const getAribb24Configuration = (
-  args: Partial<ProviderOption>
-): ProviderOption => ({
+  args: Partial<CanvasProviderOption>
+): CanvasProviderOption => ({
   useStroke: true,
   keepAspectRatio: true,
   drcsReplacement: true,
