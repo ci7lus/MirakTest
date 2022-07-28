9733fb977bf1fbc2ebb5b256eaee5019be8cb762
ci7lus <7887955+ci7lus@users.noreply.github.com>
2022-07-28 12:58:26 +0900

    :sparkles: 字幕をTextTrackで管理するように (#29)

--- src/components/contentPlayer/SubtitleRenderer.tsx
+++ src/components/contentPlayer/SubtitleRenderer.tsx
@@ -1,94 +1,142 @@
 import { CanvasProvider } from "aribb24.js"
 import clsx from "clsx"
-import React, { memo, useEffect, useRef } from "react"
+import React, { memo, useEffect, useRef, useState } from "react"
 import { useRecoilValue, useSetRecoilState } from "recoil"
 import {
   contentPlayerAribSubtitleDataAtom,
   contentPlayerDisplayingAribSubtitleDataAtom,
-  contentPlayerPlayingTimeAtom,
+  contentPlayerIsPlayingAtom,
+  contentPlayerIsSeekableAtom,
   contentPlayerPositionUpdateTriggerAtom,
-  contentPlayerSpeedAtom,
   contentPlayerSubtitleEnabledAtom,
   contentPlayerTsFirstPcrAtom,
 } from "../../atoms/contentPlayer"
-import { contentPlayerServiceSelector } from "../../atoms/contentPlayerSelectors"
+import { globalContentPlayerPlayingContentFamily } from "../../atoms/globalFamilies"
 import { subtitleSetting } from "../../atoms/settings"
 import { SUBTITLE_DEFAULT_FONT } from "../../constants/font"
 import { tryBase64ToUint8Array } from "../../utils/string"
-import { getAribb24Configuration } from "../../utils/subtitle"
+import {
+  ProviderCue,
+  getAribb24Configuration,
+  HighResMetadataTextTrack,
+} from "../../utils/subtitle"
 
 export const CoiledSubtitleRenderer: React.FC<{
   internalPlayingTimeRef: React.MutableRefObject<number>
 }> = memo(({ internalPlayingTimeRef }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  const speed = useRecoilValue(contentPlayerSpeedAtom)
   const isSubtitleEnabled = useRecoilValue(contentPlayerSubtitleEnabledAtom)
   const setDisplayingAribSubtitleData = useSetRecoilState(
     contentPlayerDisplayingAribSubtitleDataAtom
   )
-  const selectedService = useRecoilValue(contentPlayerServiceSelector)
-  const positionUpdateTrigger = useRecoilValue(
-    contentPlayerPositionUpdateTriggerAtom
-  )
-  const latestTimer = useRef<NodeJS.Timer>()
-  useEffect(() => {
-    // リセット処理
-    setDisplayingAribSubtitleData(null)
-    latestTimer.current && clearTimeout(latestTimer.current)
-    const canvas = canvasRef.current
-    if (!canvas) return
-    canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height)
-  }, [selectedService, positionUpdateTrigger])
   const setting = useRecoilValue(subtitleSetting)
 
-  // 字幕ペイロード更新時にパースしたデータをレンダリングする
   const aribSubtitleData = useRecoilValue(contentPlayerAribSubtitleDataAtom)
   const firstPcr = useRecoilValue(contentPlayerTsFirstPcrAtom)
-  const playingTime = useRecoilValue(contentPlayerPlayingTimeAtom)
-  const displayingSubtitle = useRef("")
+
+  // キューリストリセット/キャンバスリセットタイミング依存
+  const [cueTrackDep, setCueTrackDep] = useState(0)
+  const cueTrack = useRef<HighResMetadataTextTrack | null>()
   useEffect(() => {
-    const canvas = canvasRef.current
-    if (!canvas || !firstPcr) return
-    const context = canvas.getContext("2d")
-    if (!context) return
-    if (!aribSubtitleData) {
-      context.clearRect(0, 0, canvas.width, canvas.height)
+    const track = new HighResMetadataTextTrack(internalPlayingTimeRef)
+    track.startPolling()
+    cueTrack.current = track
+
+    const onCueChanged = () => {
+      const canvas = canvasRef.current
+      if (!canvas) {
+        return
+      }
+      const ctx = canvas.getContext("2d")
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+      }
       setDisplayingAribSubtitleData(null)
-      return
-    }
-    const decoded = tryBase64ToUint8Array(aribSubtitleData.data)
-    if (!decoded) return
-    const fromZero = ((aribSubtitleData.pts * 9) / 100 - firstPcr) / 90_000
-    const pts =
-      fromZero - (internalPlayingTimeRef.current || playingTime) / 1000
-    const provider = new CanvasProvider(decoded, pts)
-    const estimate = provider.render()
-    // 10秒以上先の表示は通常ではありえないので無視する
-    if (!estimate || 10 < estimate.startTime) {
-      return
-    }
-    const ctx = canvas.getContext("2d")
-    if (!ctx) return
-    const font = setting.font || SUBTITLE_DEFAULT_FONT
-    latestTimer.current = setTimeout(() => {
-      provider.render(
+      const lastCue = track.activeCues[track.activeCues.length - 1] as
+        | ProviderCue
+        | undefined
+      if (!lastCue) {
+        return
+      }
+      const playingTime = internalPlayingTimeRef.current / 1_000
+      if (playingTime < lastCue.startTime || lastCue.endTime < playingTime) {
+        return
+      }
+      const font = setting.font || SUBTITLE_DEFAULT_FONT
+      lastCue.provider.render(
         getAribb24Configuration({
           canvas,
           normalFont: font,
           gaijiFont: font,
         })
       )
-      setDisplayingAribSubtitleData(decoded)
-      displayingSubtitle.current = aribSubtitleData.data
-      if (estimate.endTime === Number.POSITIVE_INFINITY) return
-      setTimeout(() => {
-        if (displayingSubtitle.current !== aribSubtitleData.data) return
+      // https://github.com/monyone/aribb24.js/blob/f3167d4b0fd82969ac4a314be57df29648637322/src/canvas-renderer.ts#L418
+      for (let i = track.activeCues.length - 2; i >= 0; i--) {
+        const cue = track.activeCues[i]
+        cue.endTime = Math.min(cue.endTime, lastCue.startTime)
+        if (cue.startTime === cue.endTime) {
+          // .. if duplicate subtitle appeared
+          track.removeCue(cue)
+        }
+      }
+      setDisplayingAribSubtitleData(lastCue.data)
+    }
+
+    track.addEventListener("cuechange", onCueChanged)
+
+    return () => {
+      track.removeEventListener("cuechange", onCueChanged)
+      track.stopPolling()
+      cueTrack.current = null
+      const canvas = canvasRef.current
+      if (!canvas) {
+        return
+      }
+      const ctx = canvas.getContext("2d")
+      if (ctx) {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        displayingSubtitle.current = ""
-        setDisplayingAribSubtitleData(null)
-      }, ((estimate.endTime - estimate.startTime) / speed) * 1000)
-    }, (estimate.startTime / speed) * 1000)
+      }
+      setDisplayingAribSubtitleData(null)
+    }
+  }, [cueTrackDep])
+  const playingContent = useRecoilValue(
+    globalContentPlayerPlayingContentFamily(window.id ?? 0)
+  )
+  const positionUpdateTrigger = useRecoilValue(
+    contentPlayerPositionUpdateTriggerAtom
+  )
+  // コンテンツか位置が更新されたらトラックリセット
+  useEffect(() => {
+    setCueTrackDep(performance.now())
+  }, [playingContent, positionUpdateTrigger])
+  const isSeekable = useRecoilValue(contentPlayerIsSeekableAtom)
+  const isPlaying = useRecoilValue(contentPlayerIsPlayingAtom)
+  // 再生開始時に長さ不定の場合はplayingTimeリセットされるので合わせてトラックリセット
+  useEffect(() => {
+    if (isPlaying && !isSeekable) {
+      setCueTrackDep(performance.now())
+    }
+  }, [isPlaying])
+
+  // 字幕更新時にキュー挿入
+  useEffect(() => {
+    const track = cueTrack.current
+    if (!track || !aribSubtitleData) {
+      return
+    }
+    const decoded = tryBase64ToUint8Array(aribSubtitleData.data)
+    if (!decoded) {
+      return
+    }
+    const fromZero = ((aribSubtitleData.pts * 9) / 100 - firstPcr) / 90_000
+    const provider = new CanvasProvider(decoded, fromZero)
+    const estimate = provider.render()
+    if (!estimate) {
+      return
+    }
+    const cue = new ProviderCue(provider, estimate, decoded)
+    track.addCue(cue)
   }, [aribSubtitleData])
 
   return (
